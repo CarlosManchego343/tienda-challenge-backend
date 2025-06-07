623b06808e6c124ca6587ec6fc2f831eb3f52d57
Carlos Mario Manchego Pabon <carlosmariomanchego1@gmail.com>
2025-06-07 11:01:43 -0500

    Update-Server on-CMMP

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello world!!!")
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+
+    const { url, method } = req;
+
+    console.log("URL", url, " - Method: ", method);
+
+    res.writeHead(200, {"content-type": "text/plain"});
+    res.write("Received");
+    res.end();
+})
+
+server.listen(3001);
+console.log("Sever on port", 3001);
